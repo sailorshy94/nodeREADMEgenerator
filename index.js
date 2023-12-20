2fb3e17727fb4d12fb11b9ce6fc80887c54ad0f6
sailorshy94 <sbutle94@gmail.com>
2023-12-19 20:19:29 -0500

    added createPromptModule() method to inquirer const and assigned to a const variable named questions, started writing prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// TODO: Include packages needed for this application
 // added fs so can interact w/ file sys to create file w/ README data
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = inquirer.createPromptModule();
+
+prompt([
     {
         
     }
-];
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
