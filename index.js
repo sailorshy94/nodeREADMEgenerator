bc008c909358bfd137cdab1024d23ecaca7562e1
sailorshy94 <sbutle94@gmail.com>
2023-12-19 19:49:45 -0500

    added const variable for fs so can use it to interact w/ file sys

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// TODO: Include packages needed for this application
+// added fs so can interact w/ file sys to create file w/ README data
+const fs = require('fs');
 const inquirer = require('inquirer');
-// console.log(inquirer);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
