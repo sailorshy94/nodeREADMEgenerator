af24f76aed5e5a201dddc22f1bb6d158562e0420
sailorshy94 <sbutle94@gmail.com>
2023-12-19 20:28:58 -0500

    added .DS_store/ gitignore file, created more prompts in array

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-// TODO: Include packages needed for this application
 // added fs so can interact w/ file sys to create file w/ README data
 const fs = require('fs');
 const inquirer = require('inquirer');
 
 
 // TODO: Create an array of questions for user input
+// 
 const questions = inquirer.createPromptModule();
 
-prompt([
+questions([
     {
-        
-    }
+       type: 'input',
+       message: 'What is the project title?',
+       name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please add a description for your project...(Why did you create this project/what problem does it solve/what did you learn?)',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: '',
+    },
 ])
+.then((response) => {
+    console.log(response);
+});
+return;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
